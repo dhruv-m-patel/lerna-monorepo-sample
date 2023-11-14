90e12260cfda3a04ea5f58a222bfde0868d91956
Dhruv Patel <dhruv.patel@pdisoftware.com>
2023-11-14 11:06:02 -0600

    chore: split build command

--- web-app/src/common/router.tsx
+++ web-app/src/common/router.tsx
@@ -8,7 +8,7 @@ export default function Router() {
       <Route
         exact
         path="/"
-        component={loadable(() => import('./components/HomePage'))}
+        component={loadable(/* #LOADABLE */ () => import('./components/HomePage'))}
       />
     </Switch>
   );
